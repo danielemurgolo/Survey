0c53c15f7943e13324120cdc1ba126f5036225c8
danielemurgolo <daniele.murgolo@protonmail.com>
2023-08-19 09:42:04 +0200

    refractored admin view

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-)e%)klg_qd5u%_@o#&p+ok5%efx3gt7s-h0x_vy-hj(&fy70tk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "nested_inline",
     "cities_light",
     "survey.apps.SurveyConfig",
     "django_extensions",
@@ -118,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR, 'static']
+STATICFILES_DIRS = [BASE_DIR, "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- survey/admin.py
+++ survey/admin.py
@@ -1,9 +1,42 @@
 from django.contrib import admin
+from nested_inline.admin import NestedStackedInline, NestedModelAdmin
 from .models import Survey, RadioQuestion, RadioAnswer, CountryQuestion, IntegerQuestion
 
-# Register your models here.
 
-admin.site.register(Survey)
+# Inline class for RadioAnswer
+class RadioAnswerInline(NestedStackedInline):
+    model = RadioAnswer
+    extra = 1
+
+
+# Inline class for RadioQuestion with RadioAnswerInline included
+class RadioQuestionInline(NestedStackedInline):
+    model = RadioQuestion
+    extra = 1
+    inlines = [RadioAnswerInline]  # Include RadioAnswerInline here
+
+
+# Inline classes for other question types
+class CountryQuestionInline(NestedStackedInline):
+    model = CountryQuestion
+    extra = 1
+
+
+class IntegerQuestionInline(NestedStackedInline):
+    model = IntegerQuestion
+    extra = 1
+
+
+class SurveyAdmin(NestedModelAdmin):
+    inlines = [
+        RadioQuestionInline,
+        IntegerQuestionInline,
+        CountryQuestionInline,
+    ]
+
+
+# Register models
+admin.site.register(Survey, SurveyAdmin)
 admin.site.register(RadioQuestion)
 admin.site.register(RadioAnswer)
 admin.site.register(IntegerQuestion)
